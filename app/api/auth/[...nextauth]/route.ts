d43345d6085db000d8d4f65a3f2985fed4e9481c
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-10-17 19:06:02 +0200

    try correction github connexion

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '../../../../env';
 
 const handler = NextAuth({
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
         }),
         // Ajoutez d'autres fournisseurs d'authentification si nécessaire
     ],
