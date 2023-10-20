162d04f2d753965f3fe5ad0df6a33501d7a8bfed
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-10-20 15:52:11 +0200

    deployed URL nextauth changed for online

--- env.js
+++ env.js
@@ -3,4 +3,6 @@ require("dotenv").config();
 module.exports = {
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 };
