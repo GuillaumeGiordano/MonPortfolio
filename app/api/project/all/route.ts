21de7db2f1571ec85875b96f1bcf01279bd7a3b5
GuillaumeGiordano <guigoo.giordano@gmail.com>
2023-10-30 09:36:59 +0100

    deploy N3 dev & start OK

--- app/api/project/all/route.ts
+++ app/api/project/all/route.ts
@@ -1,22 +1,11 @@
 import Project from "@models/project"
 import { connectToDB } from "@util/database"
 import { NextResponse } from "next/server"
-// import { v2 as cloudinary } from "cloudinary";
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.API_KEY,
-//     api_secret: process.env.API_SECRET,
-// });
 // export const dynamic = "force-dynamic";
-// export const revalidate = 0;
+export const revalidate = 0;
 export const GET = async (request: Request) => {
     try {
-
-        // POUR UPDATE FROM CLOUDNARY !!!!
-        // const { resources } = await cloudinary.search.expression(
-        //     'folder:portfolio/*'
-        // ).sort_by('created_at', 'desc').max_results(500).execute()
 
         await connectToDB()
         const projects = await Project.find({})
